76f1a1e6c6acb51c805a1a9f1ec750d8e06e5f5c
ChrizDope <incubusid@googlemail.com>
2024-05-24 11:19:13 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 2, y = 3;
+
+        System.out.println(add(x, y));
+
+        System.out.println(sub(x, y));
+
+    }
+
+    public static int add(int x, int y){
+        return x + y;
+    }
+
+    public static int sub(int x, int y){
+        return x - y;
+    }
+
+    public static String convertToUppercase(String str){
+        return str.toUpperCase();
+    }
+
+    public static boolean isEven (int x){
+        if(x % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean isPositive(int x){
+        if (x < 0)
+            return false;
+        else
+            return true;
+    }
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void add_shouldReturn6_whenCalledWith3And3() {
+        int expected = 6;
+        assertEquals(expected, Main.add(3,3));
+    }
+
+    @Test
+    void sub_shouldReturn5_whenCalledWith8And3() {
+        int expected = 5;
+        assertEquals(expected, Main.sub(8,3));
+    }
+
+    @Test
+    void convertToUppercase_shouldReturnTEXT_whenCalledWithtext() {
+        String expected = "TEXT";
+        assertEquals(expected, Main.convertToUppercase("text"));
+    }
+
+    @Test
+    void isEven_shouldReturnTrue_whenCalledWith2() {
+        assertTrue(Main.isEven(2));
+    }
+
+    @Test
+    void isEven_shouldReturnFalse_whenCalledWith1() {
+        assertFalse(Main.isEven(1));
+    }
+
+    @Test
+    void isPositive_shouldReturnTrue_whenCalledWith2() {
+        assertTrue(Main.isEven(2));
+    }
+
+    @Test
+    void isPositive_shouldReturnFalse_whenCalledWith1() {
+        assertFalse(Main.isEven(-1));
+    }
+}
